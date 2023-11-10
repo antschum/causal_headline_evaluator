540cd77b27ea328da921bb3446abe77f93919dda
mdemireva <mdemireva@student.ethz.ch>
2023-11-10 23:08:53 +0100

    updated pairing

--- filter_pairs.py
+++ filter_pairs.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from itertools import combinations
 import pickle as pkl
+import os
 # This is preparing the Upworthy Dataset
 
 # Load data
-df = pd.read_csv("/Users/tonia/Dropbox/2023WS_Ash_Research_Causal_Predictor/osfstorage-archive/upworthy-archive-datasets/upworthy-archive-confirmatory-packages-03.12.2020.csv", low_memory=False)
-
+df = pd.read_csv("C:/Users/mldem/Downloads/upworthy-archive-datasets/upworthy-archive-confirmatory-packages-03.12.2020.csv",low_memory=False)
 # add index as column value
 df.reset_index(inplace=True)
 
@@ -14,7 +14,7 @@ df.reset_index(inplace=True)
 df['headline_count'] = df.groupby(['clickability_test_id', 'eyecatcher_id']).headline.transform('count')
 
 df_pairs = df.loc[df['headline_count']>=2, ['clickability_test_id', 'excerpt', 'headline', 'lede', 'eyecatcher_id', 'clicks', 'headline_count']]
-
+df_pairs.columns
 # probs not necessary - still have the original indices. 
 # df_pairs.reset_index(inplace=True)
 
@@ -28,17 +28,14 @@ combo = df.groupby(['clickability_test_id', 'eyecatcher_id'])['index'].apply(com
                      .stack().apply(pd.Series)\
                      .set_axis(['Idx_Headline1','Idx_Headline2'],axis = 1)\
                      .reset_index(level=0).reset_index()
-
+combo["click_difference"][0]
 combo['click_difference'] = df.loc[combo['Idx_Headline1'], ['clicks']].to_numpy()-df.loc[combo['Idx_Headline2'], ['clicks']].to_numpy()
 combo['headline1_more_clicks'] = combo['click_difference']>=0
 
+condition = combo["click_difference"] < 0
+combo.loc[condition, ['Idx_Headline1', 'Idx_Headline2']] = combo.loc[condition, ['Idx_Headline2', 'Idx_Headline1']].values   
 
 combo.drop('level_1',axis=1)
 
 
-combo.to_csv("headline_pair_indices.csv")
-# Check what we did in excel
-# combo.to_clipboard()
-
-# Now, we need to get the embeddings, and then we can step through the data and give the embeddings. 
-
+combo.to_csv("C:/Projects/CausalClicker/causal_headline_evaluator/headline_pair_indices.csv")
