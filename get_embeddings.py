7262b8fe0a6b7b2197ecef9616b971bae5627098
antschum <32771191+antschum@users.noreply.github.com>
2023-12-21 13:29:37 +0100

    calculate new embeddigns based on the reduced df

--- get_embeddings.py
+++ get_embeddings.py
@@ -1,6 +1,4 @@
 import torch 
-
-
 import pickle
 from sentence_transformers import SentenceTransformer
 import pandas as pd
@@ -21,20 +19,55 @@ else:
     device = torch.device("cpu")
 
 #Define model - pretrained
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+model = SentenceTransformer('all-mpnet-base-v2')
 
 #Load data
-df = pd.read_csv("/Users/tonia/Dropbox/2023WS_Ash_Research_Causal_Predictor/osfstorage-archive/upworthy-archive-datasets/upworthy-archive-confirmatory-packages-03.12.2020.csv", low_memory=False)
+df = pd.read_csv("upworthy-archive-confirmatory-packages-03.12.2020.csv", low_memory=False)
 #Delete some unnecessary columns
 df.columns
 delete_cols = ["created_at","updated_at","share_text","square"]
 df = df.drop(columns=delete_cols)
 
+#remove rows without eyecatcher_id (about 100)
+has_eyecatcher_id = df['eyecatcher_id'].notna()
+df = df.loc[has_eyecatcher_id]
+#Create a new column for clickrate
+df["clickrate"] = round((df["clicks"]/ df["impressions"]), ndigits=10)
+
+#filter data based on same clickability_id and eyecatcher_id
+df['headline_count'] = df.groupby(['clickability_test_id', 'eyecatcher_id']).headline.transform('count')
+df.columns
+# filter for all headlines with at least 2 pairs. 
+df = df.loc[df['headline_count']>=2, ['clickability_test_id', 'excerpt', 'headline', 'lede', 'eyecatcher_id', 'clicks', 'headline_count',"clickrate","impressions"]]
+
+# drop all rows with same headline, clickability_test_id and eyecatcher_id
+cti = df[df.duplicated(subset=["headline","clickability_test_id","eyecatcher_id"],keep=False)].clickability_test_id
+eti = df[df.duplicated(subset=["headline","clickability_test_id","eyecatcher_id"],keep=False)].eyecatcher_id
+print(df.shape)
+print("we removed: ",((df['clickability_test_id'].isin(cti) & df['eyecatcher_id'].isin(eti))).sum())
+print(df[((df['clickability_test_id'].isin(cti) & df['eyecatcher_id'].isin(eti)))][['clickability_test_id', 'eyecatcher_id', 'headline']][:20])
+df = df[~(df['clickability_test_id'].isin(cti) & df['eyecatcher_id'].isin(eti))]
+print(df.shape)
+
+#checking if it was successful
+# -> could there be cases where there are duplicates and we only delete the headlines and not the whole experiment?
+print(df[df["clickability_test_id"] == "546de9399ad54eca4800003c"]) #this is an example of matching headline, clickability test id and eyecatcher id
+df = df.sort_values(by='headline_count', ascending=False)
+##this is a new version
+dupl_headline = df[df.duplicated(subset=["headline"])] 
+
+#duplicated = df[dupl_headline]
+ids = dupl_headline["clickability_test_id"]
+eid = dupl_headline["eyecatcher_id"]
+
+mask = df['clickability_test_id'].isin(ids)&df['eyecatcher_id'].isin(eid)
+df = df[~mask]
+
 #Extract only headlines
-#headlines =df.headline.values
+headlines =df.headline.values
 
 #Embeddings
-#embeddings = model.encode(headlines,convert_to_tensor=True,batch_size=32,show_progress_bar=True)
+embeddings = model.encode(headlines,convert_to_tensor=True,batch_size=32,show_progress_bar=True)
 
-with open('full_embeddings.pkl', "wb") as fOut:
+with open('duplicates_removed_embeddings.pkl', "wb") as fOut:
      pickle.dump({'headlines': headlines, 'embeddings': embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
